8dea50372a15437032ace246d0d5d3015f072944
Yanyan <lisa.renyanyan@gmail.com>
2019-12-11 00:43:16 -0500

    added pooling layer

--- detector.py
+++ detector.py
@@ -8,6 +8,10 @@ from preprocess import get_data
 import math
 import os
 import sklearn.model_selection as sk
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from numpy import asarray
+from numpy import savetxt
+from numpy import loadtxt
 
 class Model(tf.keras.Model):
     """
@@ -35,14 +39,16 @@ class Model(tf.keras.Model):
     def call(self, inputs):
         embeddings = self.E(inputs)
         lstm1_output, lstm1_state1, lstm1_state2 = self.lstm1(embeddings)
-        att_output = self.att(lstm1_output)
+        att_output = self.att([lstm1_output, lstm1_output])
         lstm2_output, lstm2_state1, lstm2_state2 = self.lstm2(att_output)
-        final_output = self.D(lstm2_output)
+        temp = tf.keras.layers.GlobalAveragePooling1D()(lstm2_output)
+        final_output = self.D(temp)
+        # final_output = self.D(lstm2_output)
 
-        #TODO: where to put softmax? how to return the possibility?
         return final_output
 
     def loss(self, logits, labels):
+        # import pdb; pdb.set_trace()
         return tf.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(labels, logits))
 
     def accuracy(self, logits, labels):
@@ -53,35 +59,29 @@ class Model(tf.keras.Model):
 def train(model, train_inputs, train_labels):
     optimizer = tf.keras.optimizers.Adam(learning_rate=model.learning_rate)
 
-    for i in range(0, len(train_inputs), model.batch_size*model.window_size):
-        curr_inputs = np.array(train_inputs[i:i+(model.batch_size*model.window_size)])
-        batch = int(curr_inputs.shape[0] / model.window_size)
-        curr_inputs = curr_inputs[:batch*model.window_size]
-        curr_labels = train_labels[i:i+(batch*model.window_size)]
-
-        curr_inputs = np.reshape(curr_inputs, [batch, model.window_size])
+    for i in range(0, len(train_inputs), model.batch_size):
+        curr_inputs = np.array(train_inputs[i:i+model.batch_size])
+        curr_labels = train_labels[i:i+model.batch_size]
 
         # backprop
         with tf.GradientTape() as tape:
-            predictions, _ = model.call(curr_inputs, None)
+            predictions = model.call(curr_inputs)
             loss = model.loss(predictions, curr_labels)
+            print(i, loss)
         gradients = tape.gradient(loss, model.trainable_variables)
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 
 def test(model, test_inputs, test_labels):
     total_accuracy = []
-    for i in range(0, len(test_labels), model.batch_size*model.window_size):
-        curr_inputs = np.array(test_inputs[i:i+(model.batch_size*model.window_size)])
-        batch = int(curr_inputs.shape[0] / model.window_size)
-        curr_inputs = curr_inputs[:batch*model.window_size]
-        curr_labels = test_labels[i:i+(batch*model.window_size)]
 
-        curr_inputs = np.reshape(curr_inputs, [batch, model.window_size])
+    for i in range(0, len(train_inputs), model.batch_size):
+        import pdb; pdb.set_trace()
+        curr_inputs = np.array(train_inputs[i:i+model.batch_size])
+        curr_labels = train_labels[i:i+model.batch_size]
 
         predictions, _ = model.call(curr_inputs)
         total_accuracy.append(model.accuracy(predictions, curr_labels))
-
     return sum(total_accuracy) / len(total_accuracy)
 
 def main():
@@ -98,26 +98,30 @@ def main():
         sources like : Stanford Treebank Dataset, IMDB Reviews
         Dataset, Amazon Product Reviews and Sentiment 140.
     """
-    pos_vec, neg_vec, sarc_vec, vocab, pad_token_idx = get_data('./data/sentiment/P.txt', './data/sentiment/N.txt', './data/sentiment/S.txt')
-    print("finished data processing")
-    # initialize model and tensorflow variables
+    # pos_vec, neg_vec, sarc_vec, vocab, pad_token_idx = get_data('./data/sentiment/P.txt', './data/sentiment/N.txt', './data/sentiment/S.txt')
+    # print("finished data processing")
+    # # initialize model and tensorflow variables
+    #
+    # normal = np.concatenate((pos_vec, neg_vec))
+    # sarc_labels = np.ones(sarc_vec.shape[0])
+    # norm_labels = np.zeros(normal.shape[0])
+    #
+    # inputs = np.concatenate((normal, sarc_vec))
+    # labels = np.concatenate((norm_labels, sarc_labels))
 
-    pos_vec = np.array(pos_vec)
-    neg_vec = np.array(neg_vec)
-    sarc_vec = np.array(sarc_vec)
+    # # save as csv file
+    # inputs = asarray(inputs)
+    # labels = asarray(labels)
+    # savetxt('inputs.csv', inputs, delimiter=',')
+    # savetxt('labels.csv', labels, delimiter=',')
 
-    normal = np.concatenate((pos_vec, neg_vec))
-    sarc_labels = np.ones(sarc_vec.shape[0])
-    norm_labels = np.zeros(normal.shape[0])
-
-    inputs = np.concatenate((normal, sarc_vec))
-    labels = np.concatenate((norm_labels, sarc_labels))
+    inputs = loadtxt('inputs.csv', delimiter=',')
+    labels = loadtxt('labels.csv', delimiter=',')
 
     train_x, test_x, train_y, test_y = sk.train_test_split(inputs, labels, test_size=0.2, random_state = 42)
 
-    import pdb; pdb.set_trace()
-
-    model = Model(len(vocab))
+    # model = Model(len(vocab))
+    model = Model(108137)
 
     # Set-up the training step
     train(model, train_x, train_y)
